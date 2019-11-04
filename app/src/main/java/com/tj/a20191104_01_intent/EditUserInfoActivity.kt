5c1b5c5a0e4e2cf1c1c23cfa0f01d453437ae128
mikemskim <mskim@steconsulting.co.kr>
2019-11-04 19:59:25 +0900

    확인버튼 => 입력값을 intent에 담아서, 결과로 전송

--- app/src/main/java/com/tj/a20191104_01_intent/EditUserInfoActivity.kt
+++ app/src/main/java/com/tj/a20191104_01_intent/EditUserInfoActivity.kt
@@ -1,5 +1,7 @@
 package com.tj.a20191104_01_intent
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_edit_user_info.*
@@ -9,6 +11,14 @@ class EditUserInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_user_info)
+
+        okBtn.setOnClickListener {
+
+            var intent = Intent()
+            intent.putExtra("inputName",nameEdit.text.toString())
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
 
         cancelBtn.setOnClickListener {
             finish() // 해당 엑티비티 종료
